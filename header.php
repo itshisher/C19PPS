4cd63516b6d7bd2a9086799ea1e82edb26a937cf
shrutipareek <06spareek@gmail.com>
2022-07-31 23:59:45 -0400

    updated navbar

--- header.php
+++ header.php
@@ -22,6 +22,7 @@ session_start();
                 <li><a href="index.php">Home</a></li>
                 <li><a href="index_admin.php">Adminstrators</a></li>
                 <li><a href="index_researcher.php">Researchers</a></li>
+				<li><a href="articles.php">Articles</a></li>
                 <li><a href="index_orgDelegate.php">Delegates</a></li>
                 <?php
                     // check if useruid is exist in the website to make sure users are logged in
