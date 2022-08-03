87cd23767d73b770b4bf04208871c78912287388
unknown <caoyangtommy@hotmail.com>
2022-08-03 10:10:28 -0400

    link select-queries.php worked by @Dannick to index page now viewers and users can see all the queries

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <h1>COVID-19 Pandemic Progress System</h1>
         <p>The C19PPS system help researchers, companies, and world population to keep track of the COVID-19 pandemic progress.</p>
         
+        <a href="select-queries.php">Select to view all the queries</a>
         
     </section>
     
